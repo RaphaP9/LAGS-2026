5833390502fba57992708eae28537334522f46e3
Raphael <115659111+RaphaP9@users.noreply.github.com>
2026-03-27 14:36:26 -0500

    ADD ScheduleUI

--- Assets/Scripts/Systems/Schedule/ScheduleActivitySingleUI.cs
+++ Assets/Scripts/Systems/Schedule/ScheduleActivitySingleUI.cs
@@ -1,16 +1,41 @@
+using TMPro;
 using UnityEngine;
 
 public class ScheduleActivitySingleUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Components")]
+    [SerializeField] private TextMeshProUGUI performedText;
+    [SerializeField] private TextMeshProUGUI separatorText;
+    [SerializeField] private TextMeshProUGUI totalText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Settings")]
+    [SerializeField] private Color doneColor;
+    [SerializeField] private Color neutralColor;
+    [SerializeField] private Color notDoneColor;
+
+    public void SetUI(ActivitySchedulePerformed activitySchedulePerformed)
     {
-        
+        totalText.text = activitySchedulePerformed.times.ToString();
+        descriptionText.text = activitySchedulePerformed.description.ToString();
+
+        bool done = activitySchedulePerformed.timesPerformed >= activitySchedulePerformed.times;
+
+        if (done)
+        {
+            performedText.text = activitySchedulePerformed.times.ToString();
+
+            performedText.color = doneColor;
+            separatorText.color = doneColor;
+            totalText.color = doneColor;
+        }
+        else
+        {
+            performedText.text = activitySchedulePerformed.timesPerformed.ToString();
+
+            performedText.color = neutralColor;
+            separatorText.color = notDoneColor;
+            totalText.color = notDoneColor;
+        }
     }
 }
--- Assets/Scripts/Systems/Schedule/SchedulePanelUI.cs
+++ Assets/Scripts/Systems/Schedule/SchedulePanelUI.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SchedulePanelUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Components")]
+    [SerializeField] private Transform activitiesContainer;
+    [SerializeField] private Transform activityPrefab;
+
+    private void OnEnable()
     {
-        
+        ScheduleManager.OnScheduleManagerInitialized += ScheduleManager_OnScheduleManagerInitialized;
+        ScheduleManager.OnScheduleChange += ScheduleManager_OnScheduleChange;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        ScheduleManager.OnScheduleManagerInitialized -= ScheduleManager_OnScheduleManagerInitialized;
+        ScheduleManager.OnScheduleChange -= ScheduleManager_OnScheduleChange;
     }
+
+    private void UpdateUI(List<ActivitySchedulePerformed> activitiesSchedulePerformed)
+    {
+        ClearContainer();
+
+        foreach(ActivitySchedulePerformed activitySchedulePerformed in activitiesSchedulePerformed)
+        {
+            CreateSinglePrefab(activitySchedulePerformed);
+        }
+    }
+
+    private void ClearContainer()
+    {
+        foreach(Transform child in activitiesContainer)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void CreateSinglePrefab(ActivitySchedulePerformed activitySchedulePerformed)
+    {
+        Transform prefabTransform = Instantiate(activityPrefab, activitiesContainer);
+
+        ScheduleActivitySingleUI scheduleActivitySingleUI = prefabTransform.GetComponent<ScheduleActivitySingleUI>();
+
+        if (scheduleActivitySingleUI == null) return;
+
+        scheduleActivitySingleUI.SetUI(activitySchedulePerformed);
+    }
+
+    #region Subscriptions
+    private void ScheduleManager_OnScheduleManagerInitialized(object sender, ScheduleManager.OnActivityScheduleEventArgs e)
+    {
+        UpdateUI(e.activitiesPerformedList);
+    }
+
+    private void ScheduleManager_OnScheduleChange(object sender, ScheduleManager.OnActivityScheduleEventArgs e)
+    {
+        UpdateUI(e.activitiesPerformedList);
+
+    }
+    #endregion
 }
